1ab3229b31f8a9f0370a120c17db5b204d51e3b4
Smit <smitbhansali7@gmail.com>
2022-12-29 18:09:22 +0530

    home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { FaEnvelope, FaInstagram, FaLinkedinIn, FaWhatsapp, FaPhoneAlt, FaFacebookF } from 'react-icons/fa'
 import earth from '../../assets/earth.jpg'
 import './Home.css'
 
 const Home = () => {
     return (
-        <div className='flex flex-col justify-start items-start w-full'>
+        <div className='flex flex-col w-full'>
             <div className='w-full'>
                 <img src={earth} alt="earth" className='earthimage' />
             </div>
@@ -17,11 +18,45 @@ const Home = () => {
                     It is very important for the customer to pay attention to the adipiscing process. Life will never be different. Or do they not know that the wise man will rebuke the inflexibility of the born, whence the result is that the blinded inventor will often be surprised?
                 </div>
             </div>
-            <div className='container2 flex flex-col justify-end items-end text-white text-3xl font-sans pt-40 p-3'>
+            <div className='bg-black flex flex-col justify-end items-end text-white text-3xl font-sans pt-40 p-3'>
                 <span className='pr-52'>What is the space association?</span>
                 <div className='text-white text-xl font-sans p-3 m-3 w-1/2'>
                     It is very important for the customer to pay attention to the adipiscing process. Life will never be different. Or do they not know that the wise man will rebuke the inflexibility of the born, whence the result is that the blinded inventor will often be surprised?
                 </div>
+            </div>
+            <div className='flex flex-col justify-center items-center w-full bg-black pt-20'>
+                <div className='text-white text-3xl m-3 p-3'>
+                    Contact Us
+                </div>
+                <iframe
+                    className='w-full h-96 m-3 p-3'
+                    title='bvmaddress'
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3684.7105367330255!2d72.9216363148818!3d22.552513585193108!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e4e74c03b7749%3A0xab364c66fd4834c!2sBirla%20Vishvakarma%20Mahavidyalaya%20(BVM)!5e0!3m2!1sen!2sin!4v1648932714346!5m2!1sen!2sin" />
+            </div>
+            <hr />
+            <div className='bg-black p-5 flex flex-row justify-evenly items-center'>
+                <a href="mailto:">
+                    <FaEnvelope className='fill-white' />
+                </a>
+                <a href="tel:+">
+                    <FaPhoneAlt className='fill-white' />
+                </a>
+                <a href="#" target="_blank" rel="noopener noreferrer">
+                    <FaInstagram className='fill-white' />
+                </a>
+                <a href="#" target="_blank" rel="noopener noreferrer">
+                    <FaLinkedinIn className='fill-white' />
+                </a>
+                <a href="#" target="_blank" rel="noopener noreferrer">
+                    <FaWhatsapp className='fill-white' />
+                </a>
+                <a href="#" target="_blank" rel="noopener noreferrer">
+                    <FaFacebookF className='fill-white' />
+                </a>
+            </div>
+            <hr />
+            <div className='p-3 bg-black text-white flex justify-center items-center text-sm'>
+                © 2022 The Space Association, BVM. All Rights Reserved.
             </div>
         </div>
     )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,27 +1,28 @@
 import React from 'react'
 import './Navbar.css'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
         <div className='flex flex-row justify-between items-center w-full glassmorphism sticky top-5 z-10'>
-            <div className='text-white text-3xl m-5'>
-                TSA
+            <div className='text-cyan-300 text-3xl m-5 cursor-pointer'>
+                <Link to='/'>TSA</Link>
             </div>
             <div className='flex flex-row'>
-                <div className='text-white text-xl m-5'>
-                    Events
+                <div className='text-cyan-300 text-xl m-5 cursor-pointer'>
+                    <Link to='/events'>Events</Link>
                 </div>
-                <div className='text-white text-xl m-5'>
-                    Team
+                <div className='text-cyan-300 text-xl m-5 cursor-pointer'>
+                    <Link to='/team'>Team</Link>
                 </div>
-                <div className='text-white text-xl m-5'>
-                    Gallery
+                <div className='text-cyan-300 text-xl m-5 cursor-pointer'>
+                    <Link to='/gallery'>Gallery</Link>
                 </div>
-                <div className='text-white text-xl m-5'>
-                    Login
+                <div className='text-cyan-300 text-xl m-5 cursor-pointer'>
+                    <Link to='/login'>Login</Link>
                 </div>
-                <div className='text-white text-xl m-5'>
-                    Signup
+                <div className='text-cyan-300 text-xl m-5 cursor-pointer'>
+                    <Link to='/signup'>Signup</Link>
                 </div>
             </div>
         </div>
